404eef390927851ed27ae4a010925eef9ad01f6c
MMTMAA <mmtm7655@gmail.com>
2023-03-16 04:36:36 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,17 +6,15 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-	
-	printf("char size: %lu \n"(unsigned long)sizeof(a));
-	printf("int size: %lu \n"(unsigned long)sizeof(b));
-	printf("long int size: %lu \n"(unsigned long)sizeof(c));
-	printf("long long int size: %lu \n"(unsigned long)sizeof(d));
-	printf("float size: %lu \n"(unsigned long)sizeof(e));
-
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("char size: %lu \n", (unsigned long)sizeof(a));
+printf("int size: %lu \n", (unsigned long)sizeof(b));
+printf("long int size: %lu \n", (unsigned long)sizeof(c));
+printf("long long int size: %lu \n", (unsigned long)sizeof(d));
+printf("float size: %lu \n", (unsigned long)sizeof(e));
 return (0);
 }
