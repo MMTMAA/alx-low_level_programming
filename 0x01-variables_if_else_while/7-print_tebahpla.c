7e062cac6e3bc18516e7c654787654a1d036b3a1
MMTMAA <mmtm7655@gmail.com>
2023-03-17 12:03:14 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str[26] = "zyxwvutsrqponmlkjihgfedcba";
+int i = 0;
+while (i < 26)
+{
+putchar(str[i]);
+i++;
+}
+putchar('\n');
+return (0);
+}
