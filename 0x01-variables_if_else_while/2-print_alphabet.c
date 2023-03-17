de1380e35c28afdaa1a9f7ac392477e821b2b836
MMTMAA <mmtm7655@gmail.com>
2023-03-17 11:18:00 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str[100] = "abcdefghijklmnopqrstuvwxyz";
+int i = 0;
+while (i < strlen(str))
+{
+putchar(str[i]"\n");
+i++;
+}
+	return (0);
+}
