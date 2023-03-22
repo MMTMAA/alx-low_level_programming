6c6df1a18d1fe0b21d3c27ac38aac3fccf47645a
MMTMAA <mmtm7655@gmail.com>
2023-03-22 13:31:12 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * print_last_digit - Checks for lowercase charcters
+ * @c: The characters to be checked
+ * Return: 1 for lowercase characters or 0 for anything else
+ */
+
+int print_last_digit(int c)
+{
+int n = c % 10;
+return (n);
+}
