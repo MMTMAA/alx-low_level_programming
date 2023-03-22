ae1186d5362a9dc60085a1c253c5ebd88aae71de
MMTMAA <mmtm7655@gmail.com>
2023-03-22 12:47:18 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - Checks for lowercase charcters
+ * @c: The characters to be checked
+ * Return: 1 for lowercase characters or 0 for anything else
+ */
+int _isalpha(int c)
+{
+if((c <= 90 && c >= 65) || (c <= 122 && c >= 97))
+{
+return (1);
+}
+return (0);
+}
