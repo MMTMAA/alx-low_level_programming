07f7ab60d9ca448b67890b84543e95fb1ac74be7
MMTMAA <mmtm7655@gmail.com>
2023-03-23 06:08:02 -0700

    11-print_to_98.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _isdigit - Checks for lowercase charcters
+ * _isupper - Checks for lowercase charcters
  * @c: The characters to be checked
  * Return: 1 for lowercase characters or 0 for anything else
  */
-int _isdigit(int c)
+int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
 {
