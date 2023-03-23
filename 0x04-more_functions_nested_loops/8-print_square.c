7e97a495a74e735c64fcc5245088233397d5686b
MMTMAA <mmtm7655@gmail.com>
2023-03-23 09:01:47 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,25 @@
-
+#include "main.h"
+/**
+ * print_square - Checks for lowercase charcters
+ * @size: The characters to be checked
+ */
+void print_square(int size)
+{
+int i;
+int j;
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
