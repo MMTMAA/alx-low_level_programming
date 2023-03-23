079fe2f74a812800e28badaadbc4fcdd68228b28
MMTMAA <mmtm7655@gmail.com>
2023-03-23 05:41:16 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - Checks for lowercase charcters
+ * @c: The characters to be checked
+ * Return: 1 for lowercase characters or 0 for anything else
+ */
+int _isdigit(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+return (0);
+{
