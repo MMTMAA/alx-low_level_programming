5e719c8b9794b723dfa912eff7cab753dc3db6e8
MMTMAA <mmtm7655@gmail.com>
2023-03-23 07:37:15 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers - Checks for lowercase charcters
+ */
+void more_numbers(void)
+{
+int i;
+int j;
+for (j = 0; j < 10; j++)
+}
+for (i = 0; i < 15; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
+}
