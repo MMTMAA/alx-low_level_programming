4325296fd39e0fc1933822953ac2cf8e35d58259
MMTMAA <mmtm7655@gmail.com>
2023-03-27 09:03:16 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - ;ksfj
+ * @str: sfgsf
+ */
+void puts2(char *str)
+{
+int l = 0;
+int i;
+while (str[l] != '\0')
+{
+l++;
+}
+for (i = 0; i < l; l += 2)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
