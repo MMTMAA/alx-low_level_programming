751e71f345221c3e52954bdbe0c337e9e08f77a9
MMTMAA <mmtm7655@gmail.com>
2023-03-27 08:10:30 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,2 +1,21 @@
 #include "main.h"
-
+/**
+ * print_rev - sdhfjlsd
+ * @s: lhdfgljdhj
+ */
+void print_rev(char *s)
+{
+int length = 0;
+int i;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+for (i = lenhth; i > 0; i--)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+}
