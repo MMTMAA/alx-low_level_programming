2c570f42895be886314bdb95f2fc30062847ebb3
MMTMAA <mmtm7655@gmail.com>
2023-03-27 04:29:44 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - jhdfjs
+ * @s: vfjjldf
+ * return : fklvdlkfjm
+ */
+int _strlen(char *s)
+{       
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
