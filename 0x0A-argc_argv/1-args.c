ebac3cd9cd489c7060d92716097f11d9933de786
MMTMAA <mmtm7655@gmail.com>
2023-04-10 14:07:29 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - dfgdf
+ * @argc: dfgdfg
+ * @argv: dfgdf
+ * Return: hjkk
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
