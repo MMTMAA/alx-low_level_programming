e7f68a34d4f474586a9ebab6617021f2c7bfa68e
MMTMAA <mmtm7655@gmail.com>
2023-04-10 14:00:44 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - dfgdfg
+ * @argc: dfgdfg
+ * @argv: dfgdf
+ * Return: dvbdfb
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
