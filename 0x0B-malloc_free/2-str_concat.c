54c696fa390b83c8d194e40e79be98a41fd5f75a
MMTMAA <mmtm7655@gmail.com>
2023-04-11 10:48:25 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * str_concat - sdhfsdj
+ * @s1: sdfsdf
+ * @s2: sfsdfd
+ * Return: skfgsjkl
+ */
+char *str_concat(char *s1, char *s2)
+{
+int len1, len2;
+int i = 0;
+char *p;
+int x;
+
+if (s1 == NULL)
+return (NULL);
+if (s2 == NULL)
+return (NULL);
+
+len1 = strlen(s1);
+len2 = strlen(s2);
+p = malloc(sizeof(char) * (len1 + len2 + 1));
+
+if (p == NULL)
+return (NULL);
+
+while (i < len1)
+{
+p[i] = s1[i];
+i++;
+}
+x = len1 + len2;
+while (i < x)
+{
+p[i] = s2[i];
+i++;
+}
+
+
+return (p);
+}
