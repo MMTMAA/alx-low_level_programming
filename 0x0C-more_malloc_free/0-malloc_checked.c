5f3bae1a315fdb068ea5c4055a1b8a306410b28a
MMTMAA <mmtm7655@gmail.com>
2023-04-13 13:34:26 -0700

    malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib>
+/**
+ * malloc_checked - sfgfg
+ * @b: sfggs
+ * Return: klsfngk
+ */
+void *malloc_checked(unsigned int b)
+{
+void *p;
+p = malloc(b);
+if (p == NULL)
+return (98);
+return (p);
+
+}
