79843b7264699ab3f513aadd8da6c4b67eb1ac7d
MMTMAA <mmtm7655@gmail.com>
2023-04-18 01:27:19 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,10 +10,10 @@ struct dog
 {
 char *name;
 float age;
-char *owner
+char *owner;
 };
 
-typedef struct dog D;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
