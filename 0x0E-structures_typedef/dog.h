d264438331f4d8f9430e6b45e3bd65c9a544b046
MMTMAA <mmtm7655@gmail.com>
2023-04-18 01:11:14 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - jsvlkdf
+ * @name: fnvkfn
+ * @age: knv
+ * @owner: skvnksdn
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner
+};
+
+#endif
