082a12aabb15294f1aeee25e0916c977e0b2e7fe
MMTMAA <mmtm7655@gmail.com>
2023-04-18 16:55:28 -0700

    marco

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - sfgsg
+ * Return: sfdggsd
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef function_h
+#define function_h
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+#endif
