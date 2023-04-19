8c8b57a35e562e1923e3488c2953d8134584a1ec
MMTMAA <mmtm7655@gmail.com>
2023-04-19 16:11:12 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+/**
+ * int_index - sdfgg
+ * @array: gdxgx
+ * @size: sfgdg
+ * @cmp: dfgdfxg
+ * Return: dfgbd
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i = 0;
+
+if (array == NULL || size <= 0 || cmp == NULL)
+{
+return (-1);
+}
+
+while (i < size)
+{
+
+if (cmp(array[i]) == 1)
+return (i);
+
+i++;
+}
+return (-1);
+}
