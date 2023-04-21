47351a381e67a38efa1cb99dc6520dd01fe57b3e
MMTMAA <mmtm7655@gmail.com>
2023-04-21 13:12:41 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ va_start(ap, n);
 
 for (i = 0; i < n; i++)
 {
-printf("%d ", va_arg(ap, unsigned int));
+printf("%d", va_arg(ap, unsigned int));
 if (separator != NULL && i != (n - 1))
 printf("%s", separator);
 
