a449bf7f8a9d42cfc2ea380d8ea975bd35051206
MMTMAA <mmtm7655@gmail.com>
2023-04-28 13:53:38 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - sdvdz
+ * @h: zxczxc
+ * Return: zxczxc
+ */
+size_t print_list(const list_t *h)
+{
+	size_t anything = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+	return (anything);
+}
