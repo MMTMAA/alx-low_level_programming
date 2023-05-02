c7669730cc25005b65f51fc56d98c3a9b231a21c
MMTMAA <mmtm7655@gmail.com>
2023-05-01 17:22:21 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "listes.h"
+/**
+ * print_listint - sldjhvjsdan
+ * @h: lvlsdmvc
+ * Return: asfsdf
+ */
+
+size_t print_listint(const listint_t *h)
+{
+
+	size_t nodes = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
