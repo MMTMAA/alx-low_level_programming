2f840b02fcd780bb391cc480cadf865e4c02fe2c
MMTMAA <mmtm7655@gmail.com>
2023-05-01 18:45:35 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *  * free_listint2 - sldjhvjsdan
+ *   * @head: lmkzclmzx
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *NextNode;
+
+	while (head)
+	{
+		NextNode = head->next;
+		free(head);
+		head = NextNode;
+	}
+	head = NULL;
+}
