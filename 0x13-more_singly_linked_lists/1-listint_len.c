edb79d4eb412e91f4fa04677b367ee0572b25374
MMTMAA <mmtm7655@gmail.com>
2023-05-01 17:30:33 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *  * listint_len - sldjhvjsdan
+ *   * @h: lvlsdmvc
+ *    * Return: asfsdf
+ */
+
+size_t listint_len(const listint_t *h)
+{
+
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
